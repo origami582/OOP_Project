178a059cc5c76eeac8bb0f808165d5aeb8a58d59
origami582 <sodamm2kp@gmail.com>
2025-12-02 16:13:55 +0700

    fix

--- buy.java
+++ buy.java
@@ -11,11 +11,12 @@ import java.util.Scanner;
 public class buy {
     public static void main(String[] args) {
         Scanner Buyy = new Scanner(System.in);
-        System.out.print("Money");
+        System.out.print("Total Price: ");
         double money = Buyy.nextDouble();
         int Stamp = (int) (money / 50);
         System.out.println("You got "+Stamp+" stamp(s)");
     }
 }
+
 
 
