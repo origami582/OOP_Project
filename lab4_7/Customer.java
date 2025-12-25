92179d5c37a39b4374a3114a1238735bdc5433fc
origami582 <sodamm2kp@gmail.com>
2025-12-25 22:01:17 +0700

    Add files via upload

---
+++ lab4_7/Customer.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4_7;
+
+public class Customer {
+    public String name ;
+    public MyDate DOB ;
+    public Account acct ;
+}
+
